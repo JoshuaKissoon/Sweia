f249d72b40fce4538cccbb7b27b5c1c928dbc94d
Joshua Kissoon <JoshuaKissoon@gmail.com>
2013-11-16 16:53:51 +0530

    @desc Lots of enhancement of the Pager Class @enhancement Added proper commenting to all methods @enhancement Renamed attributes to follow the new coding guidelines (not uploaded yet) @feature Remove the page number argument from the passed url to remove duplication of that arg in the URL @enhancement Broken down a few methods into smaller methods @enhancement Added getRowsPerPage and getPageNoUrlKey methods

--- system/classes/JPager.php
+++ system/classes/JPager.php
@@ -3,125 +3,174 @@
     /**
      * @author Joshua Kissoon
      * @date 20121211
-     * @description Class that handles pagination throughout the website
+     * @desc Class that handles pagination throughout the website
      */
+    
+    require_once CLASSES_PATH . 'URL.php';
+    
     class JPager
     {
-       /* Pageination variables */
+        /* Pageination variables */
 
-       private $rows_per_page = 20, $total_records, $num_pages, $current_page = 1;
-       public $limit_query = '', $links = array(), $links_html = "";
-       public $offset; /* Current offset to be used in SQL query to start row number retrieval from */
-       public $delta = 4;  // Number of page links to display before and after the current page
+        private $rows_per_page = 20, $total_records, $num_pages, $current_page = 1;
+        public $links = array(), $links_html = "";
+        public $offset; /* Current offset to be used in SQL query to start row number retrieval from */
+        public $delta = 4;  // Number of page links to display before and after the current page
 
-       /* URL Variables */
-       private $url = "";   // The URL to add to the pager links
-       private $urlVar = "pager_page";   // The Pager class url variable to use
+        /* URL Variables */
+        private $url = "";   // The URL to add to the pager links
+        private $pageno_url_key = "pager_page";   // The Pager class url variable to use
 
-       public function __construct($params)
-       {
-          /*
-           * Setup the pager query and links
-           * @params - all parameters are in the $params array containing
-           *  -> total_records - the total records to be displayed
-           *  -> rows_per_page - the total records to be displayed per page
-           *  -> current_page - the current page to be shown
-           *  -> delta - Number of page links to display before and after the current page
-           *  -> retHtml - Whether to return the html or an array of links
-           *  -> htmlOptions - Array with html options for the html links
-           *  -> url - URL of the page we are on
-           *  -> urlVar - The page number var to append to the url
-           */
+        /*
+         * @desc construction of the class instance
+         */
+        public function __construct()
+        {
+            return $this;
+        }
+        /*
+         * @desc Setup the pager query and links
+         * @param $params all parameters are in the $params array containing
+         *      -> total_records - the total records to be displayed
+         *      -> rows_per_page - the total records to be displayed per page
+         *      -> current_page - the current page to be shown
+         *      -> delta - Number of page links to display before and after the current page
+         *      -> html_options - Array with html options for the html links
+         *      -> url - URL of the page we are on
+         *      -> url_var - The page number var to append to the url
+         */
 
-          /* Setting basic variables */
-          if (@$params['total_records'])
-             $this->total_records = $params['total_records'];
-          else
-             return false;
-          $this->rows_per_page = (@$params['rows_per_page']) ? $params['rows_per_page'] : $this->rows_per_page;
-          $this->current_page = (@$params['current_page'] > 0) ? $params['current_page'] : $this->current_page;
-          $this->delta = (@$params['delta'] > 0) ? $params['delta'] : $this->delta;
-          $this->retHtml = isset($params['retHtml']) ? $params['retHtml'] : false;
-          $this->htmlOptions = is_array(@$params['htmlOptions']) ? $params['htmlOptions'] : array();
-          $this->total_records = $params['total_records'];
+        public function init($params)
+        {
 
-          /* Setting URL variables */
-          $this->url = ($params['url']) ? $params['url'] : $this->url;
-          if (strpos($this->url, "?") === false)
-             $this->url = $this->url . "?";
-          $this->urlVar = ($params['urlVar']) ? $params['urlVar'] : $this->urlVar;
+            $this->total_records = isset($params['total_records']) ? $params['total_records'] : $this->total_records;
+            $this->rows_per_page = isset($params['rows_per_page']) ? $params['rows_per_page'] : $this->rows_per_page;
+            $this->current_page = (@$params['current_page'] > 0) ? $params['current_page'] : $this->current_page;
+            $this->delta = (@$params['delta'] > 0) ? $params['delta'] : $this->delta;
+            $this->html_options = is_array(@$params['html_options']) ? $params['html_options'] : array();
+            $this->total_records = $params['total_records'];
+            $this->pageno_url_key = isset($params['url_var']) ? $params['url_var'] : $this->pageno_url_key;
+            
+            /* Setting URL variables */
+            $this->url = ($params['url']) ? $params['url'] : $this->url;
+            if (strpos($this->url, "?") === false)
+            {
+                $this->url = $this->url . "?";
+            }
+            
+            /* Remove the page number from the URL if it exists */
+            $url = new URL($this->url);
+            $url->removeArg($this->pageno_url_key);
+            $this->url = $url->getURL();
 
-          /* Compute paginated values */
-          $this->calculatePages();
-          if ($this->num_pages < 2)
-          {
-             /* If we have 1 or less pages here, there is no sense in having pager intervene with the code */
-             return false;
-          }
-          $this->updateQuery();
-          $this->buildLinks();
-          if (@$params['retHtml'] === true)
-             $this->generateLinksHtml();
-       }
+            /* Compute paginated values */
+            $this->calculatePages();
+            $this->buildLinks();
+        }
 
-       private function calculatePages()
-       {
-          /* Calculates the number of pages of records we will have */
-          $this->num_pages = ceil($this->total_records / $this->rows_per_page);
-       }
+        /*
+         * @desc Calculates the number of pages of records we will have 
+         */
 
-       private function updateQuery()
-       {
-          /* Based on the # rows per page and current page we generate the updated SQL */
-          if ($this->current_page > $this->num_pages)
-             $this->current_page = $this->num_pages;
-          if ($this->current_page < 1)
-             $this->current_page = 1;
+        private function calculatePages()
+        {
+            $this->num_pages = ceil($this->total_records / $this->rows_per_page);
+        }
 
-          /* calculate offset bases on rows per page and then update the query */
-          $this->offset = ($this->current_page - 1) * $this->rows_per_page;
-          $this->limit_query = " LIMIT {$this->offset}, {$this->rows_per_page}";
-       }
+        /*
+         * @desc calculate offset based on rows per page and then update the query
+         */
 
-       public function buildLinks()
-       {
-          /*
-           * This method will build pagination links
-           */
-          $this->links = array();
-          $starting_offset = (($this->current_page - $this->delta) > 0) ? ($this->current_page - $this->delta) : 1;
-          $ending_offset = (($this->current_page + $this->delta) <= $this->num_pages) ? ($this->current_page + $this->delta) : $this->num_pages;
-          for ($i = $starting_offset; $i <= $ending_offset; $i++)
-          {
-             if (($this->current_page - $i) == 1)
-             {
-                /* If this is the previous item, insert a previous link */
-                $prev = "<a href='$this->url&$this->urlVar=$i'><<</a>";
-                $this->links = array_merge(array("prev" => $prev), $this->links);
-             }
-             else if (($this->current_page - $i) == -1)
-             {
-                /* If this is the next item, save it to insert at the end of the array */
-                $next = "<a href='$this->url&$this->urlVar=$i'>>></a>";
-             }
-             $this->links[$i] = "<a href='$this->url&$this->urlVar=$i'>$i</a>";
-          }
-          if (valid(@$next))
-             $this->links["next"] = $next;
-       }
+        public function getRowOffset()
+        {
+            if ($this->current_page > $this->num_pages)
+            {
+                $this->current_page = $this->num_pages;
+            }
+            if ($this->current_page < 1)
+            {
+                $this->current_page = 1;
+            }
 
-       public function generateLinksHtml()
-       {
-          $class = (@$this->htmlOptions['class']) ? $this->htmlOptions['class'] : "pager-link";
-          $this->links_html = "<ul id='pager-links-wrapper' class='clearfix'>";
-          foreach ($this->links as $id => $link)
-          {
-             if ($id == $this->current_page)
-                $this->links_html .= "<li class='$class $id current-page'>$link</li>";
-             else
-                $this->links_html .= "<li class='$class $id'>$link</li>";
-          }
-          $this->links_html .= "</ul>";
-       }
+            return $this->offset = ($this->current_page - 1) * $this->rows_per_page;
+        }
+
+        /*
+         * @desc Returns the number of rows per page
+         */
+
+        public function getRowsPerPage()
+        {
+            return $this->rows_per_page;
+        }
+        
+        /*
+         * @desc Returns the url key for the page number
+         */
+
+        public function getPageNoUrlKey()
+        {
+            return $this->pageno_url_key;
+        }
+
+        /*
+         * @desc This method will build pagination links
+         */
+
+        public function buildLinks()
+        {
+            $this->links = array();
+            $starting_offset = (($this->current_page - $this->delta) > 0) ? ($this->current_page - $this->delta) : 1;
+            $ending_offset = (($this->current_page + $this->delta) <= $this->num_pages) ? ($this->current_page + $this->delta) : $this->num_pages;
+            for ($i = $starting_offset; $i <= $ending_offset; $i++)
+            {
+                if (($this->current_page - $i) == 1)
+                {
+                    /* If this is the previous item, insert a previous link */
+                    $prev = "<a href='$this->url&$this->pageno_url_key=$i'><<</a>";
+                    $this->links = array_merge(array("prev" => $prev), $this->links);
+                }
+                else if (($this->current_page - $i) == -1)
+                {
+                    /* If this is the next item, save it to insert at the end of the array */
+                    $next = "<a href='$this->url&$this->pageno_url_key=$i'>>></a>";
+                }
+                $this->links[$i] = "<a href='$this->url&$this->pageno_url_key=$i'>$i</a>";
+            }
+            if (valid(@$next))
+            {
+                $this->links["next"] = $next;
+            }
+        }
+
+        /*
+         * @desc Method that generates the html links
+         */
+
+        public function getLinks()
+        {
+            return $this->links_html;
+        }
+
+        public function getLinksHtmlCode()
+        {
+            $class = isset($this->html_options['class']) ? $this->html_options['class'] : "pager-link";
+            $this->links_html = "<ul id='pager-links-wrapper' class='clearfix'>";
+            foreach ($this->links as $id => $link)
+            {
+                if ($id == $this->current_page)
+                {
+                    $this->links_html .= "<li class='$class $id current-page'>$link</li>";
+                }
+                else
+                {
+                    $this->links_html .= "<li class='$class $id'>$link</li>";
+                }
+            }
+            $this->links_html .= "</ul>";
+            
+            return $this->links_html;
+        }
 
     }
+    
