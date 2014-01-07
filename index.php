f8c38fe2a24b55f0f059183c66a2e8ed92f28eba
Joshua Kissoon <JoshuaKissoon@gmail.com>
2014-01-07 14:15:16 +0530

    Moved the entire site loading process to bootstrap. Now all of JSmart upgradable components are in the system folder

--- index.php
+++ index.php
@@ -7,57 +7,6 @@
     ini_set('display_errors', TRUE);
     require_once 'system/bootstrap.inc.php';
 
-    /**
-     * @section Loading Global Variables
-     * @note This has to be done first since some modules and classes use these Global Variables
-     */
-    $URL = JPath::urlArgs();
-    
-    /**
-     * @section Loading User Data
-     */
-    if (Session::isLoggedIn())
-    {
-        if ($_SESSION['user_type'] == JSmartUser::$user_type)
-        {
-            $USER = new JSmartUser($_SESSION['uid']);
-        }
-        else
-        {
-            $USER = new JSmartUser($_SESSION['uid']);
-        }
-    }
-    else
-    {
-        $USER = new JSmartUser();
-    }
-
-    /**
-     * @section Loading Data from cookies
-     */
-    if (!Session::isLoggedIn())
-    {
-        /* If the user is not logged in, try loading the session and login data from cookies */
-        Session::loadDataFromCookies();
-    }
-
-    /**
-     * @section Load the modules for this url 
-     */
-    $handlers = JPath::getUrlHandlers();
-    foreach ($handlers as $handler)
-    {
-        if (!isset($handler['permission']) || !valid($handler['permission']))
-        {
-            /* There is no permission for this module at the current URL, just load it */
-            include JModuleManager::getModule($handler['module']);
-        }
-        else if ($USER->usesPermissionSystem() && $USER->hasPermission($handler['permission']))
-        {
-            /* If the user has the permission to access this module for this URL, load the module */
-            include JModuleManager::getModule($handler['module']);
-        }
-    }
 
     /**
      * @section Render the theme after the necessary module is finished with its operations 
