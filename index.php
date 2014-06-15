90fc455a9c1e5ce6c876f47a7528acd0f67be498
accrete <accrete@accrete>
2014-06-15 12:09:39 +0530

    my initial commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+    /*
+     * This is the website index page that handles all requests throughout the site
+     */
+    error_reporting(E_ALL | E_WARNING | E_NOTICE);
+    ini_set('display_errors', TRUE);
+    require_once 'system/bootstrap.inc.php';
+
+
+    /**
+     * @section Render the theme after the necessary module is finished with its operations 
+     */
+    $REGISTRY->renderPage();
+    exit;
+    
---
+++ system/modules/config/config.php
@@ -0,0 +1,31 @@
+<?php
+
+    /* Define config module constants */
+    define("CONFIG_URL", BASE_URL . "config/");
+    define("CONFIG_PATH", JModuleManager::getModulePath("config"));
+
+    $url = JPath::urlArgs();
+    switch (@$url[1])
+    {
+        case "modules":
+            require CONFIG_PATH . "modules.php";
+            break;
+    }
+
+    /* Set the site title */
+    $REGISTRY->setSiteTitle(JSmart::getSiteName() . " Configuration");
+
+    /* Load the admin site navbar and sidebar */
+    $navbar = new Template(CONFIG_PATH . "templates/menus/navbar");
+    $navbar->items = config_navbar();
+    $REGISTRY->addContent("header_left", $navbar->parse());
+
+
+
+    function config_navbar()
+    {
+        $items = array(
+            CONFIG_URL . "modules" => "Modules",
+        );
+        return $items;
+    }
