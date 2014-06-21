988909b86e32374fb6cb0e0624c0075b1584d987
Joshua Kissoon <JoshuaKissoon@gmail.com>
2014-06-21 19:51:07 +0530

    Added BaseConfig class to contain base configuration to be entered by the Webmaster

---
+++ site/includes/BaseConfig.php
@@ -0,0 +1,13 @@
+<?php
+
+    /**
+     * Base Configuration to be entered by the webmaster
+     *
+     * @author Joshua Kissoon
+     * @since 20140621
+     */
+    class BaseConfig
+    {
+        //put your code here
+    }
+    
