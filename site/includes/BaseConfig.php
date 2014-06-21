2a7c1b5fe5722fef49581a2929084767cda48c02
Joshua Kissoon <JoshuaKissoon@gmail.com>
2014-06-21 20:02:34 +0530

    Added theme and admin theme name constants to the BaseConfig

--- site/includes/BaseConfig.php
+++ site/includes/BaseConfig.php
@@ -15,14 +15,15 @@
         /* Home URL */
         const HOME_URL = "home";
 
-        /**
-         * @section Database Access Information
-         */
+        /* Database Access Information */
         const DB_SERVER = "localhost";
         const DB_USER = "jsmart";
         const DB_PASS = "Pass1233";
         const DB_NAME = "jsmart_test";
 
+        /* Themes Information */
+        const THEME = "default";
+        const ADMIN_THEME = "default";
 
         /* Value used to as a salt when hashing passwords */
         const PASSWORD_SALT = "K<47`5n9~8H5`*^Ks.>ie5&";
