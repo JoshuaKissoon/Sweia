5065a3dde679c3d1309e9213cabfd8192658cade
joshuakissoon <joshuakissoon@host91.hrwebservices.net>
2014-06-25 16:38:20 +0530

    Updated system class redirect functionality

--- system/classes/System.php
+++ system/classes/System.php
@@ -10,24 +10,40 @@
     {
 
         /**
-         * Redirects the user to a specified URL if no redirect_to url argument is set
+         * Redirects the user to a specified URL. 
+         * If no URL is set, redirects the user to any return URL or to the home page
          * 
          * @param [optional] $url A URL to redirect to in the case no parameter is set
          */
-        public static function redirectTo($url = null)
+        public static function redirect($url = null)
         {
-            if (isset($_GET['return_url']))
+            if (isset($url))
+            {
+                $location = $url;
+            }
+            else if (isset($_GET['return_url']))
             {
                 $location = $_GET['return_url'];
             }
             else
             {
-                $location = $url;
+                $location = SystemConfig::baseUrl();
             }
 
             /* Redirect to the specified URL */
             header("Location: $location");
             exit();
+        }
+
+        /**
+         * Redirects the user to a specified URL
+         * 
+         * @param String $url An internal URL to redirect the user to
+         */
+        public static function redirectInternal($url)
+        {
+            $redirect_url = JPath::fullUrl($url);
+            self::redirect($redirect_url);
         }
 
     }
